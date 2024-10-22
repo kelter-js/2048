fb35d1b9cce5f9c22eb9fec3906a42284fd9b33c
kelter-js <spawnkillerqw@gmail.com>
2024-10-22 14:40:05 +0500

    feat: constants

---
+++ src/constants/index.ts
@@ -0,0 +1,20 @@
+export const UP_ARROW = 38;
+export const DOWN_ARROW = 40;
+export const LEFT_ARROW = 37;
+export const RIGHT_ARROW = 39;
+export const MAX_ATTEMPTS = 16;
+export const AMOUNT_OF_ROWS_AND_COLUMNS = 4;
+export const MIN_AMOUNT_OF_ROWS_AND_COLUMNS = 2;
+export const WIN_VALUE = 2048;
+
+export const GAME_OVER_DESCRIPTION = "Игра закончена!";
+export const GAME_WIN_DESCRIPTION = "Вы выйграли!";
+
+export const INITIATE_FIELD = [
+  [0, 0, 0, 0],
+  [0, 0, 0, 0],
+  [0, 0, 0, 0],
+  [0, 0, 0, 0],
+];
+
+export const DIRECTIONS = ["left", "right", "down", "up"];
