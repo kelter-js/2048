2b1da3a18fa028dd96a6473a079dfb2d598f9c4d
kelter-js <spawnkillerqw@gmail.com>
2024-10-29 12:24:45 +0500

    feat: create one index endpoint for all component to export

---
+++ src/components/index.ts
@@ -0,0 +1,8 @@
+import { GameOverOverlay } from "./GameOverOverlay";
+import { NewGameButton } from "./NewGameButton";
+import { Statistics } from "./Statistics";
+import { Block } from "./Block";
+import { Rules } from "./Rules";
+import { Row } from "./Row";
+
+export { GameOverOverlay, NewGameButton, Statistics, Block, Rules, Row };
